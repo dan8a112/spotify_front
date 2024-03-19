60205ac2530a17f41d29eb62c41ea0fe20f184a4
Daniel <danyochoa112@gmail.com>
2024-03-19 16:20:37 -0600

    iniciando vista login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/form.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    <title>Iniciar Sesion - Spotify</title>
+</head>
+<body>
+    <header>
+        <img src="img/logo_spotify.png" alt="logo principal" id="logo">
+    </header>
+    <main>
+        <h2>Inicia sesión en Spotify</h2>
+        <hr>
+        <div class="form">
+            <form action="POST">
+                <div>
+                    <label for="">Correo Electronico</label>
+                    <input type="email" placeholder="Correo Electronico" class="input">
+                </div>
+                <div>
+                    <label for="">Contrasena</label>
+                    <input type="password" placeholder="Ingrese su contrasena" class="input">
+                </div>
+            </form>
+        </div>
+    </main>
+    <footer></footer>
+</body>
+</html>
